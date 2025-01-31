f6ee358174ec25274f77207f79fe991b98cf2565
rorikwong <108846354+rorikwong@users.noreply.github.com>
2025-01-31 10:08:44 -0600

    the start of menu-ing

--- MainMenu.Designer.cs
+++ MainMenu.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1486, 960);
+            Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(6, 6, 6, 6);
+            Name = "MainMenu";
+            Text = "TurboDash";
+            ResumeLayout(false);
         }
 
         #endregion
