e0ab26f8d9c048e9193a526e562a919a6b9a5d82
rorikwong <108846354+rorikwong@users.noreply.github.com>
2025-01-31 14:31:45 -0600

    Add Table works now

--- MainMenu.Designer.cs
+++ MainMenu.Designer.cs
@@ -23,170 +23,229 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             partname = new Label();
             partnumber = new Label();
             partstatus = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
+            partnametext = new TextBox();
+            partnumbertext = new TextBox();
+            partcosttext = new TextBox();
             partcost = new Label();
-            textBox4 = new TextBox();
             addpart = new Button();
             updatepart = new Button();
             deletebutton = new Button();
             clearbutton = new Button();
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            partsviewer = new DataGridView();
+            partstatusid = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)partsviewer).BeginInit();
             SuspendLayout();
             // 
             // partname
             // 
             partname.AutoSize = true;
             partname.BackColor = SystemColors.AppWorkspace;
-            partname.Location = new Point(31, 94);
+            partname.Location = new Point(58, 89);
+            partname.Margin = new Padding(6, 0, 6, 0);
             partname.Name = "partname";
             partname.Size = new Size(130, 32);
             partname.TabIndex = 0;
             partname.Text = "Part Name:";
-            partname.Click += label1_Click;
             // 
             // partnumber
             // 
             partnumber.AutoSize = true;
             partnumber.BackColor = SystemColors.AppWorkspace;
-            partnumber.Location = new Point(31, 170);
+            partnumber.Location = new Point(58, 242);
+            partnumber.Margin = new Padding(6, 0, 6, 0);
             partnumber.Name = "partnumber";
             partnumber.Size = new Size(154, 32);
             partnumber.TabIndex = 1;
             partnumber.Text = "Part Number:";
-            partnumber.Click += label1_Click_1;
             // 
             // partstatus
             // 
             partstatus.AutoSize = true;
             partstatus.BackColor = SystemColors.AppWorkspace;
-            partstatus.Location = new Point(31, 339);
+            partstatus.Location = new Point(58, 550);
+            partstatus.Margin = new Padding(6, 0, 6, 0);
             partstatus.Name = "partstatus";
             partstatus.Size = new Size(125, 32);
             partstatus.TabIndex = 2;
             partstatus.Text = "Part Status";
             // 
-            // textBox1
+            // partnametext
             // 
-            textBox1.Location = new Point(242, 91);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(630, 39);
-            textBox1.TabIndex = 3;
+            partnametext.Location = new Point(289, 89);
+            partnametext.Margin = new Padding(6);
+            partnametext.Name = "partnametext";
+            partnametext.Size = new Size(1167, 39);
+            partnametext.TabIndex = 3;
+            partnametext.TextChanged += partnametext_TextChanged;
             // 
-            // textBox2
+            // partnumbertext
             // 
-            textBox2.Location = new Point(242, 170);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(630, 39);
-            textBox2.TabIndex = 4;
+            partnumbertext.Location = new Point(289, 242);
+            partnumbertext.Margin = new Padding(6);
+            partnumbertext.Name = "partnumbertext";
+            partnumbertext.Size = new Size(1167, 39);
+            partnumbertext.TabIndex = 4;
+            partnumbertext.TextChanged += partnumbertext_TextChanged;
             // 
-            // textBox3
+            // partcosttext
             // 
-            textBox3.Location = new Point(242, 258);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(630, 39);
-            textBox3.TabIndex = 5;
+            partcosttext.Location = new Point(289, 370);
+            partcosttext.Margin = new Padding(6);
+            partcosttext.Name = "partcosttext";
+            partcosttext.Size = new Size(1167, 39);
+            partcosttext.TabIndex = 5;
+            partcosttext.TextChanged += partcosttext_TextChanged;
             // 
             // partcost
             // 
             partcost.AutoSize = true;
             partcost.BackColor = SystemColors.AppWorkspace;
-            partcost.Location = new Point(31, 265);
+            partcost.Location = new Point(58, 370);
+            partcost.Margin = new Padding(6, 0, 6, 0);
             partcost.Name = "partcost";
             partcost.Size = new Size(113, 32);
             partcost.TabIndex = 6;
             partcost.Text = "Part Cost:";
             // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(242, 339);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(630, 39);
-            textBox4.TabIndex = 7;
-            // 
             // addpart
             // 
-            addpart.Location = new Point(31, 496);
+            addpart.Location = new Point(31, 669);
+            addpart.Margin = new Padding(6);
             addpart.Name = "addpart";
-            addpart.Size = new Size(140, 76);
+            addpart.Size = new Size(260, 162);
             addpart.TabIndex = 8;
             addpart.Text = "Add Part";
             addpart.UseVisualStyleBackColor = true;
+            addpart.Click += addpart_Click;
             // 
             // updatepart
             // 
-            updatepart.Location = new Point(219, 496);
+            updatepart.Location = new Point(346, 669);
+            updatepart.Margin = new Padding(6);
             updatepart.Name = "updatepart";
-            updatepart.Size = new Size(140, 76);
+            updatepart.Size = new Size(260, 162);
             updatepart.TabIndex = 9;
             updatepart.Text = "Update Table";
             updatepart.UseVisualStyleBackColor = true;
+            updatepart.Click += updatepart_Click;
             // 
             // deletebutton
             // 
-            deletebutton.Location = new Point(411, 496);
+            deletebutton.Location = new Point(660, 669);
+            deletebutton.Margin = new Padding(6);
             deletebutton.Name = "deletebutton";
-            deletebutton.Size = new Size(140, 76);
+            deletebutton.Size = new Size(260, 162);
             deletebutton.TabIndex = 10;
             deletebutton.Text = "Delete";
             deletebutton.UseVisualStyleBackColor = true;
+            deletebutton.Click += deletebutton_Click;
             // 
             // clearbutton
             // 
-            clearbutton.Location = new Point(606, 496);
+            clearbutton.Location = new Point(985, 669);
+            clearbutton.Margin = new Padding(6);
             clearbutton.Name = "clearbutton";
-            clearbutton.Size = new Size(140, 76);
+            clearbutton.Size = new Size(260, 162);
             clearbutton.TabIndex = 11;
             clearbutton.Text = "Clear";
             clearbutton.UseVisualStyleBackColor = true;
+            clearbutton.Click += clearbutton_Click;
             // 
-            // dataGridView1
+            // partsviewer
             // 
-            dataGridView1.AllowUserToOrderColumns = true;
-            dataGridView1.BackgroundColor = SystemColors.ControlDarkDark;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.GridColor = Color.DimGray;
-            dataGridView1.Location = new Point(31, 612);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(873, 311);
-            dataGridView1.TabIndex = 12;
+            partsviewer.AllowUserToOrderColumns = true;
+            partsviewer.BackgroundColor = SystemColors.ControlDarkDark;
+            partsviewer.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            partsviewer.GridColor = Color.DimGray;
+            partsviewer.Location = new Point(31, 875);
+            partsviewer.Margin = new Padding(6);
+            partsviewer.Name = "partsviewer";
+            partsviewer.Size = new Size(1621, 456);
+            partsviewer.TabIndex = 12;
+            partsviewer.CellContentClick += partsviewer_CellContentClick;
+            // 
+            // partstatusid
+            // 
+            partstatusid.FormattingEnabled = true;
+            partstatusid.Location = new Point(289, 542);
+            partstatusid.Name = "partstatusid";
+            partstatusid.Size = new Size(1175, 40);
+            partstatusid.TabIndex = 13;
+            partstatusid.SelectedIndexChanged += partstatusid_SelectedIndexChanged;
             // 
             // MainMenu
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaptionText;
-            ClientSize = new Size(943, 960);
-            Controls.Add(dataGridView1);
+            ClientSize = new Size(1797, 1421);
+            Controls.Add(partstatusid);
+            Controls.Add(partsviewer);
             Controls.Add(clearbutton);
             Controls.Add(deletebutton);
             Controls.Add(updatepart);
             Controls.Add(addpart);
-            Controls.Add(textBox4);
             Controls.Add(partcost);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(partcosttext);
+            Controls.Add(partnumbertext);
+            Controls.Add(partnametext);
             Controls.Add(partstatus);
             Controls.Add(partnumber);
             Controls.Add(partname);
             Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Margin = new Padding(6, 6, 6, 6);
+            Margin = new Padding(6);
             Name = "MainMenu";
             Text = "TurboDash";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)partsviewer).EndInit();
             ResumeLayout(false);
             PerformLayout();
+        }
+        private void partnametext_TextChanged(object sender, EventArgs e)
+        {
+            // Handle text changed event
+        }
+
+        private void partnumbertext_TextChanged(object sender, EventArgs e)
+        {
+            // Handle text changed event
+        }
+
+        private void partcosttext_TextChanged(object sender, EventArgs e)
+        {
+            // Handle text changed event
+        }
+
+        private void updatepart_Click(object sender, EventArgs e)
+        {
+            // Handle update part button click event
+        }
+
+        private void deletebutton_Click(object sender, EventArgs e)
+        {
+            // Handle delete button click event
+        }
+
+        private void clearbutton_Click(object sender, EventArgs e)
+        {
+            // Handle clear button click event
+        }
+
+        private void partsviewer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Handle parts viewer cell content click event
+        }
+
+        private void partstatusid_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Handle part status id selected index changed event
         }
 
         #endregion
@@ -194,15 +253,15 @@
         private Label partname;
         private Label partnumber;
         private Label partstatus;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
+        private System.Windows.Forms.TextBox partnametext;
+        private TextBox partnumbertext;
+        private TextBox partcosttext;
         private Label partcost;
-        private TextBox textBox4;
         private Button addpart;
         private Button updatepart;
         private Button deletebutton;
         private Button clearbutton;
-        private DataGridView dataGridView1;
+        private DataGridView partsviewer;
+        private ComboBox partstatusid;
     }
 }
--- MainMenu.cs
+++ MainMenu.cs
@@ -5,16 +5,30 @@ namespace turbo_dash
         public MainMenu()
         {
             InitializeComponent();
+
+            // Set up the DataGridView columns
+            partsviewer.Columns.Add("PartName", "Part Name");
+            partsviewer.Columns.Add("PartNumber", "Part Number");
+            partsviewer.Columns.Add("PartCost", "Part Cost");
+            partsviewer.Columns.Add("PartStatus", "Part Status");
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void addpart_Click(object sender, EventArgs e)
         {
+            // Get the values from the text fields
+            string partName = partnametext.Text;
+            string partNumber = partnumbertext.Text;
+            string partCost = partcosttext.Text;
+            string partStatus = partstatusid.Text;
 
-        }
+            // Add the values as a new row in the DataGridView
+            partsviewer.Rows.Add(partName, partNumber, partCost, partStatus);
 
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
+            // Optional: Clear the text fields after adding the row
+            partnametext.Text = "";
+            partnumbertext.Text = "";
+            partcosttext.Text = "";
+            partstatusid.Text = "";
         }
     }
 }
